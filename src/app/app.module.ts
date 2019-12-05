bd0d9f5e6b67a4c825a657d4b81389016c63d832
yipriyadarshini <yindirapriyadarshini@gmail.com>
2019-12-05 15:33:11 +0530

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { ServerComponent } from "./server/server.component";
-import { TestComponent } from './test/test.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+import { FailalertComponent } from './failalert/failalert.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ServerComponent, TestComponent],
+  declarations: [AppComponent, HelloComponent, ServerComponent, SuccessalertComponent, FailalertComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/failalert/failalert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-failalert',
+  templateUrl: './failalert.component.html',
+  styleUrls: ['./failalert.component.css']
+})
+export class FailalertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
